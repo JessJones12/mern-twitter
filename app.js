05a02029fdc75f2b9e006eaa4058358adf065024
Jess <jess.jonesr@gmail.com>
2021-02-28 16:47:29 -0500

    set up user routes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -10,6 +13,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hiii"));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
